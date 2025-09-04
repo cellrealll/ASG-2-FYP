6822f77f6445e1eab4e7e79cd518d98a2b73d084
Marcella <marcellafeliciasalim@gmail.com>
2025-09-04 20:54:20 +0700

    ASG2FYP

---
+++ ASG2FYP.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Haloo, nama saya Marcella Felicia Salim dengan NIM = 2902617453. Saya menyukai bahasa pemrograman python.");
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    cout<<"Masukkan angka 1: ";
+    cin >> x;
+    cout<<"Masukkan angka 2: ";
+    cin>>y;
+    cout<<"Hasilnya adalah: ";
+    cout << x + y<< endl;
+    return 0;
+}
+
